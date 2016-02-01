ac488907959dd332810f744b31e677db2b537600
Anton Makrushyn <ansermak@gmail.com>
2016-02-01 13:14:53 +0200

    Add lists app

---
+++ lists/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page(request):
+    return HttpResponse(
+        '''<html>
+            <head>
+                <title>To-Do lists</title>
+            </head>
+            <body>
+            </body>
+        </html>''')
