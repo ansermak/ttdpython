f0c0d10c89b4e5895abf19e3a95893f216ed35e9
Anton Makrushyn <ansermak@gmail.com>
2016-02-04 17:42:05 +0200

    Split out unit tests on model and view tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -56,34 +56,6 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.list, list_)
 
 
-class ListViewTest(TestCase):
-    def test_uses_list_template(self):
-        list_ = List.objects.create()
-        response = self.client.get('/lists/{}/'.format(list_.id))
-        self.assertTemplateUsed(response, 'lists/list.html')
-
-    def test_displays_all_items(self):
-        correct_list = List.objects.create()
-        Item.objects.create(text='itemey 1', list=correct_list)
-        Item.objects.create(text='itemey 2', list=correct_list)
-        other_list = List.objects.create()
-        Item.objects.create(text='other list item 1', list=other_list)
-        Item.objects.create(text='other list item 2', list=other_list)
-
-        response = self.client.get('/lists/{}/'.format(correct_list.id))
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
-        self.assertNotContains(response, 'other list item 1')
-        self.assertNotContains(response, 'other list item 2')
-
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get('/lists/{}/'.format(correct_list.id))
-        self.assertEqual(response.context['list'], correct_list)
-
-
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
         self.client.post(
